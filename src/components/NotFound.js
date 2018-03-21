f230ba8b4acd4a03a995655b699eb9ea7bd5d686
PcCodeWarrior <PC_Code_Warrior@yahoo.com>
2018-03-21 16:27:17 -0600

    add React Router

---
+++ src/components/NotFound.js
@@ -0,0 +1,15 @@
+/**
+ * Created by tom on 3/21/2018.
+ */
+
+import React from "react";
+
+const NotFound = () =>(
+    <div>
+        <h2>
+            Not Found!!!!!
+        </h2>
+    </div>
+)
+
+export default NotFound;
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,14 +2,27 @@
  * Created by tom on 3/21/2018.
  */
 import React from 'react';
+import {getFunName} from '../helpers';
 
 class StorePicker extends React.Component {
+
+    myInput = React.createRef();
+
+goToStore = (event) => {
+
+    event.preventDefault();
+    const storeName = this.myInput.value.value
+    this.props.history.push(`/store/${storeName}`);
+}
+
+
     render() {
         return (
-            <form action="" className="store-picker">
+            <form className="store-selector" onSubmit={this.goToStore}>
                 {/* this is what a comment looks like*/}
             <h2>Please Enter A Store </h2>
-                <input type="text" required placeholder="Store Name" />
+
+                <input type="text" ref={this.myInput} required placeholder="Store Name" defaultValue={getFunName()} />
                 <button type="submit">Visit Store</button>
         </form>
         )
